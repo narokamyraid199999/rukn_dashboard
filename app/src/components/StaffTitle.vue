9f8a0eb23ce7cbf0ea97e813a7b699f01dddee15
narokamyraid199999 <143643793+narokamyraid199999@users.noreply.github.com>
2024-11-08 13:14:26 -0800

    feat: add staff title

--- app/src/components/StaffTitle.vue
+++ app/src/components/StaffTitle.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div>
+  <div class="flex items-center justify-between">
     <div>
       <div class="text-[#696969] text-lg">
         <RouterLink class="" to="/">الصفحة الرئيسية </RouterLink>
@@ -9,6 +9,52 @@
         <span class="select-none">></span>
         HR-EMP-00001
       </div>
+      <div class="flex gap-x-6 items-center">
+        <p class="text-xl mt-2">أحمد سامي علي</p>
+        <p
+          class="bg-[#28B8701A] text-[#28B870] px-6 relative top-2 text-lg rounded-md select-none"
+        >
+          نشط
+        </p>
+      </div>
+    </div>
+
+    <div class="flex items-center gap-x-3">
+      <button
+        class="bg-[#28B870] text-[#FFFFFF] py-2 px-5 flex justify-center items-center outline-none rounded-lg"
+      >
+        حفظ
+      </button>
+      <button
+        class="bg-[#FFFFFF] icon-shadow py-2 px-3 flex justify-center items-center"
+      >
+        <span :class="{ 'material-icons ': true }">arrow_forward_ios</span>
+      </button>
+      <button
+        class="bg-[#FFFFFF] icon-shadow py-2 px-3 flex justify-center items-center"
+      >
+        <span :class="{ 'material-icons ': true }">arrow_back_ios</span>
+      </button>
+      <button
+        class="bg-[#FFFFFF] icon-shadow py-2 px-3 flex justify-center items-center"
+      >
+        <span :class="{ 'material-icons ': true }">ios_share</span>
+      </button>
+      <button
+        class="bg-[#FFFFFF] shadow-md icon-shadow py-1 px-3 flex justify-center items-center"
+      >
+        <span :class="{ 'material-icons icon': true }">more_horiz</span>
+      </button>
     </div>
   </div>
 </template>
+
+<style scoped>
+.icon {
+  font-size: 2rem;
+}
+
+.icon-shadow {
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.05);
+}
+</style>
