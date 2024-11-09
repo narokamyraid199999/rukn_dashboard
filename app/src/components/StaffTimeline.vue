194385e764c4d92f3a9ca235f912dc9944858559
narokamyraid199999 <143643793+narokamyraid199999@users.noreply.github.com>
2024-11-09 13:15:05 -0800

    fix: timeline margin

--- app/src/components/StaffTimeline.vue
+++ app/src/components/StaffTimeline.vue
@@ -10,7 +10,7 @@
         >
         </span>
 
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+        <p class="text-base relative -top-1 text-gray-500 dark:text-gray-400">
           Get started with dozens of web components and interactive elements
           built on top of Tailwind CSS.
         </p>
@@ -21,30 +21,32 @@
         >
         </span>
 
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+        <p class="text-base relative -top-1 text-gray-500 dark:text-gray-400">
           Get started with dozens of web components and interactive elements
           built on top of Tailwind CSS.
         </p>
       </li>
       <li class="mb-14 ms-6">
-        <span
-          class="absolute flex items-center justify-center w-3 h-3 bg-[#E84646] rounded-full -start-1.5 ring-white dark:ring-[#E84646] dark:bg-[#E84646]"
-        >
-        </span>
-
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-          Get started with dozens of web components and interactive elements
-          built on top of Tailwind CSS.
-        </p>
-      </li>
-      <li class="ms-6">
         <span
           class="absolute flex items-center justify-center w-3 h-3 bg-[#E84646] rounded-full -start-1.5 ring-white dark:ring-[#E84646] dark:bg-[#E84646]"
         >
         </span>
 
         <p
-          class="text-base font-normal text-gray-700 dark:text-gray-700 text-sm"
+          class="text-base font-normal relative -top-1 text-gray-500 dark:text-gray-400"
+        >
+          Get started with dozens of web components and interactive elements
+          built on top of Tailwind CSS.
+        </p>
+      </li>
+      <li class="ms-9">
+        <span
+          class="absolute flex items-center justify-center w-3 h-3 bg-[#E84646] rounded-full -start-1.5 ring-white dark:ring-[#E84646] dark:bg-[#E84646]"
+        >
+        </span>
+
+        <p
+          class="relative -top-1 relative -top-1 text-gray-700 dark:text-gray-700 text-sm"
         >
           لقد قمت بأنشائها منذ شهر واحد * لقد قمت بتحرير هذا منذ شهر واحد
         </p>
